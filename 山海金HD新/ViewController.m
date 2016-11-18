167b78d52c69f5180ddb4701cf8472a5e70441f7
ios <ios@iosdemacbook-pro.local>
2016-11-18 09:52:18 +0800

    Initial Commit

---
+++ 山海金HD新/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  山海金HD新
+//
+//  Created by ios on 16/11/18.
+//  Copyright © 2016年 ytshanghai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ 山海金HD新/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  山海金HD新
+//
+//  Created by ios on 16/11/18.
+//  Copyright © 2016年 ytshanghai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
